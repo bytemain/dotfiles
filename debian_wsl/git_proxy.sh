8deb0c1bc8c46af8e877ab7d7f3e9927cfa33a48
Artin <lengthmin@gmail.com>
2019-08-03 21:56:18 +0800

    Update(Debian) git-proxy

--- debian_wsl/git_proxy.sh
+++ debian_wsl/git_proxy.sh
@@ -1,3 +1,6 @@
-git config --global http.https://github.com.proxy http://127.0.0.1:7890
+#winip="127.0.0.1"
+winip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
+proxy="http://${winip}:7890"
+git config --global http.https://github.com.proxy ${proxy}
 sudo apt-get install -y netcat-openbsd
 cat ../ssh.proxy > ~/.ssh/config 
