6d8adfcd2b9aa3af9bf5a8cd9cc579bebe6ff821
Artin <lengthmin@gmail.com>
2019-12-23 10:29:41 +0800

    Update git_proxy

--- ubuntu_wsl/git_proxy.sh
+++ ubuntu_wsl/git_proxy.sh
@@ -3,5 +3,11 @@ winip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
 proxy="http://${winip}:7890"
 ssh_proxy="${winip}:7891"
 git config --global http.https://github.com.proxy ${proxy}
-sudo apt-get install -y netcat-openbsd > /dev/null 2>&1
-cat ~/dotfiles/_rc/ssh.proxy | sed "s/_ssh_proxy_/$ssh_proxy/g"  > ~/.ssh/config 
+if ! grep -qF "Host github.com" ~/.ssh/config ; then
+    cat ~/dotfiles/_rc/ssh.proxy | sed "s/_ssh_proxy_/$ssh_proxy/g"  >> ~/.ssh/config
+else
+    lino=$(($(awk '/Host github.com/{print NR}'  ~/.ssh/config)+2))
+    sed -i "${lino}c\    ProxyCommand nc -X 5 -x $ssh_proxy %h %p" ~/.ssh/config
+fi
+
+
