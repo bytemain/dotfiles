26819574b405a17aba989eb91952588bd923dced
Artin <lengthmin@gmail.com>
2020-05-28 12:10:36 +0800

    init codespaces

---
+++ codespaces/init.vim
@@ -0,0 +1,150 @@
+filetype on
+filetype indent on
+syntax on
+" Protect changes between writes. Default values of
+" updatecount (200 keystrokes) and updatetime
+" (4 seconds) are fine
+set swapfile
+set directory^=~/.vim/swap//
+
+" protect against crash-during-write
+set writebackup
+" but do not persist backup after successful write
+set nobackup
+" use rename-and-write-new method whenever safe
+set backupcopy=auto
+" patch required to honor double slash at end
+set backupdir^=~/.vim/backup//
+" persist the undo tree for each file
+set undofile
+set undodir^=~/.vim/undo//
+set cmdheight=1
+set updatetime=300
+set signcolumn=yes
+set listchars=tab:>-,trail:~,extends:>,precedes:<,nbsp:-
+set list
+set number
+set nospell
+set hlsearch
+set smartcase
+set encoding=utf-8
+set tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+set copyindent
+set autoindent
+set cursorline
+set cursorcolumn
+set shortmess+=cfilmnrxoOtT
+set showmatch
+set showmode
+set showcmd
+set clipboard+=unnamedplus
+set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}
+set guicursor=n-v-c:block,i-ci-ve:ver10,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175
+
+" make tab do tabs at beginning and spaces elsewhere
+function RetabIndents()
+  let l:saved_view = winsaveview()
+  silent! %s/\t/\=repeat(" ", &tabstop)/
+  call winrestview(l:saved_view)
+endfunction
+
+" http://vim.wikia.com/wiki/Remove_unwanted_spaces
+function StripTrailingWhite()
+  let l:winview = winsaveview()
+  silent! %s/\s\+$//
+  call winrestview(l:winview)
+endfunction
+
+autocmd BufWritePre,FileAppendPre,FileWritePre,FilterWritePre * :call StripTrailingWhite()
+autocmd BufWritePre,FileAppendPre,FileWritePre,FilterWritePre * :call RetabIndents()
+
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+endif
+
+augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave * set rnu
+    autocmd BufLeave,FocusLost,InsertEnter   * set nornu
+augroup END
+
+call plug#begin(stdpath('data') . '/plugged')
+Plug 'junegunn/vim-easy-align'
+Plug 'SirVer/ultisnips'
+Plug 'honza/vim-snippets'
+Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+Plug 'preservim/nerdcommenter'
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'jackguo380/vim-lsp-cxx-highlight'
+Plug 'Chiel92/vim-autoformat'
+Plug 'dracula/vim', { 'as': 'dracula' }
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+call plug#end()
+
+let g:python3_host_prog = '/home/linuxbrew/.linuxbrew/opt/python@3.8/bin/python3'
+let g:UltiSnipsExpandTrigger="<c-u><c-n>"
+let g:UltiSnipsJumpForwardTrigger="<c-b>"
+let g:UltiSnipsJumpBackwardTrigger="<c-z>"
+let g:UltiSnipsEditSplit="vertical"
+
+map <C-b> :NERDTreeToggle<CR>
+
+set termguicolors
+colorscheme dracula
+set background=dark
+
+map  <silent> <S-Insert> "+p
+imap <silent> <S-Insert> <Esc>"+pa
+
+inoremap <silent><expr> <TAB>
+      \ pumvisible() ? coc#_select_confirm() :
+      \ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
+      \ <SID>check_back_space() ? "\<TAB>" :
+      \ coc#refresh()
+
+function! s:check_back_space() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+let g:coc_snippet_next = '<tab>'
+inoremap <expr> <Tab> pumvisible() ? "\<C-n>" : "\<Tab>"
+inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+inoremap <silent><expr> <c-space> coc#refresh()
+" Use <cr> to confirm completion
+inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+autocmd! CompleteDone * if pumvisible() == 0 | pclose | endif
+
+" Use `[c` and `]c` to navigate diagnostics
+nmap <silent> [c <Plug>(coc-diagnostic-prev)
+nmap <silent> ]c <Plug>(coc-diagnostic-next)
+
+" Remap keys for gotos
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gy <Plug>(coc-type-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+
+nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+function! s:show_documentation()
+    if (index(['vim','help'], &filetype) >= 0)
+        execute 'h '.expand('<cword>')
+    else
+        call CocAction('doHover')
+    endif
+endfunction
+
+" Highlight symbol under cursor on CursorHold
+autocmd CursorHold * silent call CocActionAsync('highlight')
+au CursorHoldI * sil call CocActionAsync('showSignatureHelp')
+
+command! -nargs=0 Format :call CocAction('format')
+command! -nargs=? Fold   :call CocAction('fold', <f-args>)
+command! -nargs=0 OR     :call CocAction('runCommand', 'editor.action.organizeImport')
+
+au BufRead,BufNewFile *.cpp,*.c nnoremap <F5>   <Esc>:w<CR>:!g++ -std=c++11 % -o /tmp/a.out && /tmp/a.out<CR>
+au BufRead,BufNewFile *.cpp,*.c nnoremap <F7>   <Esc>:w<CR>:!g++ -std=c++11 %<CR>
+au BufRead,BufNewFile *.cpp,*.c nnoremap <C-F5> <Esc>:w<CR>:!g++ -std=c++11 -g % -o /tmp/a.out && gdb /tmp/a.out<CR>
---
+++ codespaces/install.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+sukkaEnvVersion="VSCode Codespaces"
+
+start() {
+    clear
+    curl ip.gs
+    echo ""
+    echo "==========================================================="
+    echo "   _____       _    _            ______                    "
+    echo "  / ____|     | |  | |          |  ____|                   "
+    echo " | (___  _   _| | _| | ____ _   | |__   ______   __        "
+    echo "  \___ \| | | | |/ / |/ / _\` |  |  __| |  _ \\ \\ / /     "
+    echo "  ____) | |_| |   <|   < (_| |  | |____| | | \\ V /        "
+    echo " |_____/ \__,_|_|\\_\\_|\\_\\__,_|  |______|_| |_|\\_/     "
+    echo "                                                           "
+    echo "==========================================================="
+    echo "                !! ATTENTION !!"
+    echo "YOU ARE SETTING UP: Sukka Environment (${sukkaEnvVersion})"
+    echo "==========================================================="
+    echo ""
+    echo -n "* The setup will begin in 5 seconds... "
+    sleep 5
+    echo -n "Times up! Here we start!"
+    echo ""
+
+    cd $HOME
+}
+
+install-linux-packages() {
+    echo "==========================================================="
+    echo "* Install following packages:"
+    echo "-----------------------------------------------------------"
+
+    sudo apt-get update
+    sudo apt-get install -y python3-dev python3-pip python3-setuptools
+    sudo apt-get install -y build-essential libreadline-dev apt-file
+    sudo apt-get install -y zsh curl wget git tree unzip ncdu tmux trash-cli
+    sudo apt-get install -y festival festvox-kallpc16k
+    sudo apt-get install -y neofetch screenfetch autojump
+    sudo apt-get install -y lsof whois traceroute
+    sudo apt-get install -y net-tools iputils-tracepath dnsutils
+    sudo apt-get install -y netcat-openbsd
+
+}
+
+setup-omz() {
+    echo "==========================================================="
+    echo "                      Shells Enviroment"
+    echo "-----------------------------------------------------------"
+    echo "* Installing Oh-My-Zsh..."
+    echo "-----------------------------------------------------------"
+
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+
+    echo "-----------------------------------------------------------"
+    echo "* Installing ZSH Custom Plugins & Themes:"
+    echo ""
+    echo "  - zsh-autosuggestions"
+    echo "  - zsh-syntax-highlighting"
+    echo "-----------------------------------------------------------"
+
+    cp ~/dotfiles/codespaces/zsh_plugins.txt ~/.zsh_plugins.txt
+    cp -r ~/dotfiles/zsh-theme/. ~/.oh-my-zsh/custom/themes/
+}
+
+install-linuxbrew(){
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.zprofile
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+    brew install gcc git-quick-stats cheat
+    brew install ripgrep bat exa neovim git fzf
+    brew install getantibody/tap/antibody
+    brew install ctop hub onefetch httpie
+    brew install jesseduffield/lazygit/lazygit lazydocker
+}
+
+
+install-nodejs() {
+    install-nvm() {
+        echo "-----------------------------------------------------------"
+        echo "* Installing NVM..."
+        echo "-----------------------------------------------------------"
+
+        curl -o- https://cdn.jsdelivr.net/gh/creationix/nvm/install.sh | bash
+
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+        echo "-----------------------------------------------------------"
+        echo -n "* NVM Verision: "
+        command -v nvm
+    }
+
+    install-node() {
+        echo "-----------------------------------------------------------"
+        echo "* Installing NodeJS..."
+        echo "-----------------------------------------------------------"
+
+        nvm install node
+        nvm use node
+        echo "-----------------------------------------------------------"
+        echo -n "* NodeJS Version: "
+
+        node -v
+    }
+
+    install-yarn() {
+        yarn --version
+    }
+
+    yarn-global-add() {
+        echo "-----------------------------------------------------------"
+        echo "* Yarn Global Add those packages:"
+        echo "-----------------------------------------------------------"
+        yarn global add npm-check-updates nali-cli
+        yarn global add http-server serve
+        yarn global add what-is-x liyu wtf
+        yarn global add typescript ts-node
+    }
+
+
+    echo "==========================================================="
+    echo "              Setting up NodeJS Environment"
+
+    install-nvm
+    install-node
+    install-yarn
+    yarn-global-add
+}
+
+zshrc() {
+    echo "==========================================================="
+    echo "                  Import zshrc                   "
+    echo "-----------------------------------------------------------"
+    cd $HOME/dotfiles
+    cat ./codespaces/zshrc > $HOME/.zshrc
+}
+
+upgrade-packages() {
+    echo "==========================================================="
+    echo "                      Upgrade packages                     "
+    echo "-----------------------------------------------------------"
+
+    sudo apt-get update && sudo apt-get upgrade -y
+}
+
+finish() {
+    echo "==========================================================="
+    echo "> Do not forget run those things:"
+    echo ""
+    echo "- chsh -s /usr/bin/zsh"
+    echo "- git-config"
+    echo "- install linuxbrew: "
+    echo "==========================================================="
+}
+
+start
+install-linux-packages
+install-linuxbrew
+setup-omz
+install-nodejs
+zshrc
+upgrade-packages
+finish
