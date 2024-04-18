134774c6f73764ca19879440222e3f227f12d1eb
野声 <lijiacheng.ljc@antgroup.com>
2024-04-18 16:24:29 +0800

    remove unused archive dotfiles

--- _archive/arch_wsl/git_proxy.sh
+++
@@ -1,3 +0,0 @@
-git config --global http.https://github.com.proxy http://127.0.0.1:7890
-yay -S openbsd-netcat
-cat ../ssh.proxy > ~/.ssh/config 
--- _archive/debian_wsl/git_proxy.sh
+++
@@ -1,6 +0,0 @@
-#winip="127.0.0.1"
-winip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
-proxy="http://${winip}:7890"
-git config --global http.https://github.com.proxy ${proxy}
-sudo apt-get install -y netcat-openbsd > /dev/null 2>&1
-cat ~/dotfiles/_rc/ssh.proxy | sed "s/_winip_/$winip/g"  > ~/.ssh/config 
