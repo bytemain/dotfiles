a55ad1b196740b5043144942db96b951c6d8b8f7
野声 <lijiacheng.ljc@antgroup.com>
2024-10-10 12:29:18 +0000

    Remove wezterm

--- macos/wezterm.lua
+++
@@ -1,7 +0,0 @@
-home = os.getenv("HOME");
-base_dir = home .. '/dotfiles/wezterm/';
-
-package.path = base_dir .. '?.lua;' .. package.path;
-
-local wezterm = require(base_dir..'wezterm');
-return wezterm;
