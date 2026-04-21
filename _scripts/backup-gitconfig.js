86dc3e22ea1573bc16f025b6baac45b0502cc8d8
Jiacheng <artin@cat.ms>
2026-04-21 12:48:19 +0000

    Backup
    Added: .vfox.toml Deleted: .tool-versions Modified: .gitignore _scripts/backup-gitconfig.js

--- _scripts/backup-gitconfig.js
+++ _scripts/backup-gitconfig.js
@@ -3,21 +3,26 @@ const os = require("os");
 const gitconfig = require("./utils/git-config");
 const path = require("path");
 
-const exts = fs.readFileSync(path.join(os.homedir(), ".gitconfig"));
-console.log(`🚀 ~ file: backup-gitconfig.js ~ line 10 ~ exts`, exts.toString());
-
 const data = gitconfig.sync();
-console.log(`🚀 ~ file: backup-gitconfig.js ~ line 16 ~ data`, data);
 
 // generate git command from sectionAllowed
 // git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 // `git config` --[scope] [section].[key] [value]
 function getKeyValues(obj, prefix = "", keyValues = []) {
   for (let key in obj) {
-    const fullKey = prefix + key;
     if (typeof obj[key] === "object") {
-      getKeyValues(obj[key], fullKey + ".", keyValues);
+      // 处理带引号的 section，如 `url "git@github.com:"` -> `url."git@github.com:"`
+      let sectionKey = key;
+      if (key.includes(' ')) {
+        const parts = key.split(' ');
+        if (parts.length > 1) {
+          sectionKey = parts[0] + '.' + parts.slice(1).join(' ');
+        }
+      }
+      getKeyValues(obj[key], prefix ? prefix + sectionKey + "." : sectionKey + ".", keyValues);
     } else {
+      let fullKey = prefix + key;
+      // 移除不必要的转义
       keyValues.push([fullKey, obj[key]]);
     }
   }
@@ -29,33 +34,18 @@ function escapeValueForShell(value) {
     return value;
   }
 
-  // 检查值是否已经被双引号包围
-  const isQuoted = value.startsWith('"') && value.endsWith('"');
-  // 如果已经被引号包围，移除外层引号以便处理内容
-  if (isQuoted) {
-    // 移除外层引号
-    value = value.substring(1, value.length - 1);
-  }
+  // 移除多余的转义字符
+  value = value.replace(/\\\\/g, '\\');
 
-    // 对于普通值，转义所有特殊字符
-    value = value
-      .replace(/\\/g, '\\\\')
-      .replace(/"/g, '\\"')
-      .replace(/\$/g, '\\$')
-      .replace(/`/g, '\\`')
-      .replace(/!/g, '\\!')
-      .replace(/\[/g, '\\[')
-      .replace(/\]/g, '\\]');
-  // 返回处理后的值，并标记它是否原本就被引号包围
-  return { value, isQuoted };
+  return value;
 }
 
 function generateGitCommand(data) {
   const keyValues = getKeyValues(data);
   let cmd = "";
   keyValues.forEach(([k, v]) => {
-    const result = escapeValueForShell(v);
-    cmd += `git config --global ${k} "${result.value}"\n`;
+    const escapedValue = escapeValueForShell(v);
+    cmd += `git config --global ${k} "${escapedValue}"\n`;
   });
   return cmd;
 }
